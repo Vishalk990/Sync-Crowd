83762a4648cc38a74e08d9809a27a088d7491a0b
Vishal Kalita <vishalkalita036@gmail.com>
2024-07-10 10:21:06 +0530

    clerk auth added

---
+++ app/(dashboard)/dashboard/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import Navbar from "@/components/ui/Navbar";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default page;
--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
+"use client"
+import { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
+import { UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   const [isSheetOpen, setIsSheetOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+
+  const redirectToAuth = ()=> {
+    router.push('/dashboard');
+  }
+
+  useEffect(()=> {
+    console.log(path);
+  })
 
   return (
     <header className="container mx-auto py-8">
       <nav className="flex justify-between items-center">
         <div className="text-2xl font-bold">SyntheticData</div>
-        <div className="hidden max-sm:flex items-center">
+        <div className="flex lg:hidden items-center">
           <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
             <SheetTrigger asChild>
               <Button variant="ghost" onClick={() => setIsSheetOpen(true)}>
@@ -27,11 +40,14 @@ const Navbar = () => {
             </SheetContent>
           </Sheet>
         </div>
-        <div className="space-x-4 hidden max-sm:hidden">
+        <div className="hidden lg:flex space-x-4">
           <Button variant="ghost">Features</Button>
           <Button variant="ghost">Pricing</Button>
           <Button variant="ghost">About</Button>
-          <Button>Get Started</Button>
+          {/* <Button onClick={redirectToAuth}>Get Started</Button> */}
+          {
+            path === '/dashboard'? (<UserButton/>): (<Button onClick={redirectToAuth}>Get Started</Button>)
+          }
         </div>
       </nav>
     </header>
