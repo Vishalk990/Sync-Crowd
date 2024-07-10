25e46eb7540469a4412bde7499afc13941a26787
“SuryanshBVerma-christuniversity” <suryansh.bachchanverma@mca.christuniversity.in>
2024-07-10 11:18:50 +0530

    Edited the clerk auth form

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      layout: {
+        socialButtonsVariant: "iconButton",
+        logoImageUrl: "/icons/yoom-logo.svg",
+      },
+      variables: {
+        colorText: "#fff",
+        colorPrimary: "#0E78F9",
+        colorBackground: "#1C1F2E",
+        colorInputBackground: "#252A41",
+        colorInputText: "#fff"
+      },
+    }}>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
