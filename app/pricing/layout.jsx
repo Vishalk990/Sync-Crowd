0439a8f966ae8bdb644fe8a67796917cbb18efa3
“SuryanshBVerma-christuniversity” <suryansh.bachchanverma@mca.christuniversity.in>
2024-07-12 19:54:46 +0530

    Pricing UI

--- app/pricing/layout.jsx
+++ app/pricing/layout.jsx
@@ -5,9 +5,9 @@ import React from "react";
 
 function PricingLayout({ children }) {
   return (
-    <div>
+    <div className="bg-gradient-to-br from-blue-100 to-purple-100">
       <Navbar/>
-      <div className="mx-5 md:mx-20 lg:mx-36">{children}</div>
+      <div className="mx-5 md:mx-20 lg:mx-36 ">{children}</div>
       <Footer/>
     </div>
   );
