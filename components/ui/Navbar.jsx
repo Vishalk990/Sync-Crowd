9fa4e644be383a19d3672892b4ae84e7c76094ca
“SuryanshBVerma-christuniversity” <suryansh.bachchanverma@mca.christuniversity.in>
2024-07-13 09:52:11 +0530

    Navbar - UI Changes

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Input } from "@/components/ui/input"; // Importing the search bar input component from ShadCN
-import { Menu } from "lucide-react";
+import { Menu, Search } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
 import { NavModal } from "./NavModal";
@@ -26,11 +26,28 @@ const Navbar = () => {
     <header className="mx-auto py-8 px-10 sticky top-0 z-50 bg-gradient-to-br from-blue-100 to-purple-100 shadow-md">
       <nav className="flex justify-between items-center">
         <div
-          className="text-2xl font-bold  cursor-pointer"
+          className="text-2xl font-bold  cursor-pointer flex gap-9 items-center"
           onClick={() => router.push("/")}
         >
           SyncCrowd
+          <NavModal />
         </div>
+
+
+        <div className="hidden lg:flex space-x-4 items-center">
+
+          <Input placeholder="Search" className="w-64" />{" "} <Search />
+          {/* Adding the search bar */}
+          <Button onClick={redirectToAuth}>Login</Button>{" "}
+          {/* Adding the Login button */}
+          {path === "/dashboard" ? (
+            <UserButton />
+          ) : (
+            <Button onClick={redirectToAuth}>Get Started</Button>
+          )}
+        </div>
+
+
         <div className="flex lg:hidden items-center">
           <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
             <SheetTrigger asChild>
@@ -61,20 +78,6 @@ const Navbar = () => {
               </nav>
             </SheetContent>
           </Sheet>
-        </div>
-        
-        <div className="hidden lg:flex space-x-4 items-center">
-          <NavModal/>
-
-          <Input placeholder="Search" className="w-64" />{" "}
-          {/* Adding the search bar */}
-          <Button onClick={redirectToAuth}>Login</Button>{" "}
-          {/* Adding the Login button */}
-          {path === "/dashboard" ? (
-            <UserButton />
-          ) : (
-            <Button onClick={redirectToAuth}>Get Started</Button>
-          )}
         </div>
       </nav>
     </header>
