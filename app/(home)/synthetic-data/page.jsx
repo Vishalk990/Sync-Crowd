ca42502e9264dbec714e296cbd06baf6bab8fedf
Vishal Kalita <vishalkalita036@gmail.com>
2024-08-16 07:55:28 +0530

    fixed bugs

--- app/(home)/synthetic-data/page.jsx
+++ app/(home)/synthetic-data/page.jsx
@@ -4,7 +4,7 @@ import { CardDemo } from "@/components/custom/CardDemo";
 import { Sparkles } from "lucide-react";
 import { useRouter } from "next/navigation";
 
-const page = () => {
+const SyntheticDataPageContent = () => {
   const router = useRouter();
 
   return (
@@ -22,25 +22,30 @@ const page = () => {
         </nav>
 
         <div className="flex items-center gap-4 p-11 justify-center">
-        <CardDemo 
-          title="Generate data from a prompt" 
-          description="Use advanced LLM models to generate high-quality synthetic data from simple text prompts. Perfect for creating diverse datasets effortlessly."
-          type="1"
-          onClick={() => router.push("/synthetic-data/using-LLM")}
-        />
+          <CardDemo
+            title="Generate data from a prompt"
+            description="Use advanced LLM models to generate high-quality synthetic data from simple text prompts. Perfect for creating diverse datasets effortlessly."
+            type="1"
+            onClick={() => router.push("/synthetic-data/using-LLM")}
+          />
 
-        <CardDemo 
-          title="Generate synthetic data from CSV's" 
-          description="Provide a small dataset, and let our models generate comprehensive synthetic data, extending and enhancing your original data seamlessly." 
-          type="2"
-          onClick={() => router.push("/synthetic-data/using-generators")}
-        />
+          <CardDemo
+            title="Generate synthetic data from CSV's"
+            description="Provide a small dataset, and let our models generate comprehensive synthetic data, extending and enhancing your original data seamlessly."
+            type="2"
+            onClick={() => router.push("/synthetic-data/using-generators")}
+          />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
+const page = () => {
+  return (
+    <>
+      <SyntheticDataPageContent />
+    </>
+  );
+};
 
-export default page
-
-
+export default page;
--- components/custom/Companies.jsx
+++ components/custom/Companies.jsx
@@ -7,7 +7,7 @@ export function Companies() {
       <div className="py-14">
         <div className="container mx-auto px-4 md:px-8">
           <h3 className="text-center text-sm font-semibold text-gray-500 uppercase">
-            Powering the world's best data teams
+            Powering the world&apos;s best data teams
           </h3>
           <div className="relative mt-6">
             <div className="flex flex-wrap justify-center items-center gap-4 p-3">
